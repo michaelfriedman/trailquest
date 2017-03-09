ac95d71aa6d02bec232c021d5edb6e35287eb206
Michael Friedman <contact@michaeldavidfriedman.com>
2017-03-09 13:04:44 -0800

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,1 @@
-exports.up = knex => knex.schema.createTable('users', (table) => {
-  table.increments();
-  table.string('first_name').defaultTo('');
-  table.string('last_name').defaultTo('');
-  table.string('email').unique().notNullable();
-  table.specificType('hashed_password', 'char(60)').notNullable();
-  table.string('profile_photo_url').defaultTo('');
-  table.timestamps(true, true);
-});
 
-exports.down = knex => knex.schema.dropTable('users');
